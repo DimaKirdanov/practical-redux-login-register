5d29a19fc8778136f969ae8a645c44d866cbb308
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2023-03-17 17:15:05 +0200

    1

--- src/components/pages/UserPage/UserPage.jsx
+++ src/components/pages/UserPage/UserPage.jsx
@@ -1,5 +1,13 @@
+import { listenerCancelled } from '@reduxjs/toolkit/dist/listenerMiddleware/exceptions';
+import { useSelector } from 'react-redux';
+import { selectUsers } from 'redux/users/usersSelectors';
 export const UserPage = () => {
-    return (
-        <p>Welcome to users page</p>
-    )
-}
+  const users = useSelector(selectUsers);
+    return
+    <ul>
+        {
+            users.map(user => <li key={user.id}>
+                
+            </li>)
+    </ul>;
+};
